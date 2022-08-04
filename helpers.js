b553b89353f4071f31935261b8c36a78f00df920
LLiNDEE <linus.linde@bahnhof.se>
2022-08-04 21:10:52 +0200

    fix: Change useCustomTemplate to accept a config-path. This allows users to place/name the config file anywhere.

--- helpers.js
+++ helpers.js
@@ -1,227 +1,265 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
-import { strictMode } from "./index.js"
+import { strictMode } from './index.js';
 
-const validTemplates = ["missing_params", "invalid_params", "unknown_params"];
+const validTemplates = ['missing_params', 'invalid_params', 'unknown_params'];
 export const customTemplates = {};
+export let CONFIG_PATH = 'validator.config.yml';
 
 export const getMissingParams = (incomingKeys, expectedKeys) => {
+	const keys = Object.keys(expectedKeys);
+	const incoming_keys = Object.keys(incomingKeys);
+	const missingParams = keys.filter((k) => !incoming_keys.includes(k));
+	if (missingParams.length > 0) {
+		return {
+			success: false,
+			missingParams: missingParams,
+		};
+	}
 
-    const keys = Object.keys(expectedKeys)
-    const incoming_keys = Object.keys(incomingKeys)
-    const missingParams = keys.filter(k => !incoming_keys.includes(k))
-    if(missingParams.length > 0){
-        return {
-            success: false,
-            missingParams: missingParams
-        }
-    }
-
-    return {
-        success: true
-    }
-}
+	return {
+		success: true,
+	};
+};
 
 export const generateError = (type, config) => {
-    return {
-        type: type,
-        success: false,
-        ...config
-    }
-}
+	return {
+		type: type,
+		success: false,
+		...config,
+	};
+};
 
 export const checkInvalidParams = (incomingKeys, expectedKeys) => {
-    let isValid = true
-    const invalidParams = []
+	let isValid = true;
+	const invalidParams = [];
 
-    Object.entries(expectedKeys).forEach(([param, v]) => {
-        const rules = v.rules;
+	Object.entries(expectedKeys).forEach(([param, v]) => {
+		const rules = v.rules;
 
-        let exist = false;
-        rules.forEach(ruleObject => {
-  
-            let incomingValue = incomingKeys[param];
+		let exist = false;
+		rules.forEach((ruleObject) => {
+			let incomingValue = incomingKeys[param];
 
-            if(typeof ruleObject === 'object'){
-                if(ruleObject.rule === 'default'){
-                    if(typeof incomingValue === 'undefined' || incomingValue.trim() === ""){
-                        incomingKeys[param] = ruleObject.value;
-                    }
-                }
-            }
+			if (typeof ruleObject === 'object') {
+				if (ruleObject.rule === 'default') {
+					if (
+						typeof incomingValue === 'undefined' ||
+						incomingValue.trim() === ''
+					) {
+						incomingKeys[param] = ruleObject.value;
+					}
+				}
+			}
 
-            if(transformType[ruleObject.rule]){
-                incomingKeys[param] = transformType[ruleObject.rule](incomingValue);
-                return;
-            }
+			if (transformType[ruleObject.rule]) {
+				incomingKeys[param] = transformType[ruleObject.rule](incomingValue);
+				return;
+			}
 
-            if(typeof incomingValue === 'undefined' || incomingValue.trim() === ""){
-                if(!strictMode && typeof incomingValue === 'undefined') return;
-                if(exist) return;
-                if(checkDefaultValue(rules)) return;
-                isValid = false;
-                exist = true;
-                const formattedRules = formatRules(rules);
-                return invalidParams.push({[param]: {'expected_type(s)': formattedRules}});
-            }
+			if (typeof incomingValue === 'undefined' || incomingValue.trim() === '') {
+				if (!strictMode && typeof incomingValue === 'undefined') return;
+				if (exist) return;
+				if (checkDefaultValue(rules)) return;
+				isValid = false;
+				exist = true;
+				const formattedRules = formatRules(rules);
+				return invalidParams.push({
+					[param]: { 'expected_type(s)': formattedRules },
+				});
+			}
 
-            if(typeof ruleObject === 'object'){
-                if(ruleObject.rule === 'default'){
-                    if(incomingValue.trim() === "") incomingKeys[param] = value;
-                    return;
-                }
-                
-                if(ruleObject.rule === 'email' && ruleObject?.regex !== ""){
-                    if(!validateType[ruleObject.rule](incomingValue, ruleObject?.regex)){
-                        if(exist) return;
-                        isValid = false;
-                        exist = true;
-                        const formattedRules = formatRules(rules);
-                        return invalidParams.push({[param]: {'expected_type(s)': formattedRules}});
-                    }
-                    return;
-                }
+			if (typeof ruleObject === 'object') {
+				if (ruleObject.rule === 'default') {
+					if (incomingValue.trim() === '') incomingKeys[param] = value;
+					return;
+				}
 
-                if(ruleObject.rule === 'required') return;
-                
-                if(ruleObject?.value){
-                    if(!validateType[ruleObject.rule](incomingValue, ruleObject.value)){
-                        if(exist) return;
-                        isValid = false;
-                        exist = true;
-                        const formattedRules = formatRules(rules);
-                        return invalidParams.push({[param]: {'expected_type(s)': formattedRules}});
-                    }
-                }else{
-                    if(!validateType[ruleObject.rule](incomingValue)){
-                        if(exist) return;
-                        isValid = false;
-                        exist = true;
-                        const formattedRules = formatRules(rules);
-                        return invalidParams.push({[param]: {'expected_type(s)': formattedRules}});
-                    }
-                }
-            }
+				if (ruleObject.rule === 'email' && ruleObject?.regex !== '') {
+					if (
+						!validateType[ruleObject.rule](incomingValue, ruleObject?.regex)
+					) {
+						if (exist) return;
+						isValid = false;
+						exist = true;
+						const formattedRules = formatRules(rules);
+						return invalidParams.push({
+							[param]: { 'expected_type(s)': formattedRules },
+						});
+					}
+					return;
+				}
 
-        })
-    })
+				if (ruleObject.rule === 'required') return;
 
-    if(!isValid){
-        return {
-            success: false,
-            invalidParams: invalidParams
-        }
-    }
+				if (ruleObject?.value) {
+					if (!validateType[ruleObject.rule](incomingValue, ruleObject.value)) {
+						if (exist) return;
+						isValid = false;
+						exist = true;
+						const formattedRules = formatRules(rules);
+						return invalidParams.push({
+							[param]: { 'expected_type(s)': formattedRules },
+						});
+					}
+				} else {
+					if (!validateType[ruleObject.rule](incomingValue)) {
+						if (exist) return;
+						isValid = false;
+						exist = true;
+						const formattedRules = formatRules(rules);
+						return invalidParams.push({
+							[param]: { 'expected_type(s)': formattedRules },
+						});
+					}
+				}
+			}
+		});
+	});
 
-    return {
-        success: true,
-        transformed_keys: incomingKeys,
-    }
+	if (!isValid) {
+		return {
+			success: false,
+			invalidParams: invalidParams,
+		};
+	}
 
-}
+	return {
+		success: true,
+		transformed_keys: incomingKeys,
+	};
+};
 
 const checkDefaultValue = (rules) => {
-    let hasDefaultValue = false;
+	let hasDefaultValue = false;
 
-    rules.forEach(ruleObject => {
-        if(ruleObject.rule === 'default') return hasDefaultValue = true;
-    })
+	rules.forEach((ruleObject) => {
+		if (ruleObject.rule === 'default') return (hasDefaultValue = true);
+	});
 
-    return hasDefaultValue;
-}
+	return hasDefaultValue;
+};
 
 export const getUnknownParams = (receivedParams, expectedParams) => {
-    const unknownParams = [];
-    Object.entries(receivedParams).forEach(([key, value]) => {
-        if(!expectedParams[key]) unknownParams.push(key);
-    })
+	const unknownParams = [];
+	Object.entries(receivedParams).forEach(([key, value]) => {
+		if (!expectedParams[key]) unknownParams.push(key);
+	});
 
-    if(unknownParams.length > 0){
-        return {
-            success: false,
-            unknownParams: unknownParams,
-        }
-    }
+	if (unknownParams.length > 0) {
+		return {
+			success: false,
+			unknownParams: unknownParams,
+		};
+	}
 
-    return {
-        success: true
-    }
+	return {
+		success: true,
+	};
+};
 
-}
-
-const formatRules = rules => {
-    const formattedRules = [];
-    rules.forEach(ruleObject => {
-        if(typeof ruleObject.rule === 'string' && ruleObject.rule !== 'required'){
-            formattedRules.push(ruleObject.rule);
-        }
-        resolveInvalidMessage(ruleObject.rule, ruleObject.value) !== "" && formattedRules.push(resolveInvalidMessage(ruleObject.rule, ruleObject.value));
-    })
-    return formattedRules;
-} 
+const formatRules = (rules) => {
+	const formattedRules = [];
+	rules.forEach((ruleObject) => {
+		if (typeof ruleObject.rule === 'string' && ruleObject.rule !== 'required') {
+			formattedRules.push(ruleObject.rule);
+		}
+		resolveInvalidMessage(ruleObject.rule, ruleObject.value) !== '' &&
+			formattedRules.push(
+				resolveInvalidMessage(ruleObject.rule, ruleObject.value)
+			);
+	});
+	return formattedRules;
+};
 
 export const resolveInvalidMessage = (type, value) =>
-type === 'min' ? `Minimum length of ${value}`
-: type === 'max' ? `Maximum length of ${value}`
-: type === 'enum' ? `Allowed values: ${value}`
-: type === 'regex' ? `Did not match the regex: ${value}`
-: ""
+	type === 'min'
+		? `Minimum length of ${value}`
+		: type === 'max'
+		? `Maximum length of ${value}`
+		: type === 'enum'
+		? `Allowed values: ${value}`
+		: type === 'regex'
+		? `Did not match the regex: ${value}`
+		: '';
 
 export const validateType = {
-    string: v => isString(v) && v.trim() !== "",
-    integer: v => isNumber(+v),
-    min: (v, m) => checkMinValue(v, m),
-    max: (v, m) => checkMaxValue(v, m),
-    enum: (v, enums) => checkEnum(v, enums),
-    email: (email, regex = undefined) => checkEmail(email, regex),
-    positive: v => isPositive(v),
-    negative: v => isNegative(v),
-    regex: (v, regex) => testRegex(v, regex),
-}
+	string: (v) => isString(v) && v.trim() !== '',
+	integer: (v) => isNumber(+v),
+	min: (v, m) => checkMinValue(v, m),
+	max: (v, m) => checkMaxValue(v, m),
+	enum: (v, enums) => checkEnum(v, enums),
+	email: (email, regex = undefined) => checkEmail(email, regex),
+	positive: (v) => isPositive(v),
+	negative: (v) => isNegative(v),
+	regex: (v, regex) => testRegex(v, regex),
+};
 
 export const transformType = {
-    lowercase: v => v.toLowerCase(),
-    uppercase: v => v.toUpperCase(),
-    trim: v => v.trim(),
-}
+	lowercase: (v) => v.toLowerCase(),
+	uppercase: (v) => v.toUpperCase(),
+	trim: (v) => v.trim(),
+};
 
 export const readConfig = () => {
-    try{
-        const fileContents = fs.readFileSync('validator.config.yml', 'utf8');
-        const data = yaml.load(fileContents);
+	try {
+		if (!validateConfigPath()) {
+			return console.log(
+				`CONFIG-ERROR!\r\n Config file named '${CONFIG_PATH}' does not exist.`
+			);
+		}
 
-        if(!data || data === undefined) return;
-        setCustomTemplate(data);
-    } catch(e) {
-        return false;
-    }
-}
+		const fileContents = fs.readFileSync(CONFIG_PATH, 'utf8');
+		const data = yaml.load(fileContents);
 
-const setCustomTemplate = data => {
-    Object.entries(data).forEach(([k, v]) => {
-        if(!validTemplates.includes(k)) return;
-        customTemplates[k] = v?.template;
-    })
-}
+		if (!data || data === undefined) return;
+		setCustomTemplate(data);
+	} catch (e) {
+		return false;
+	}
+};
+
+const validateConfigPath = () => {
+	try {
+		if (!CONFIG_PATH.includes('.yml')) CONFIG_PATH = CONFIG_PATH + '.yml';
+		return fs.existsSync(CONFIG_PATH);
+	} catch (e) {
+		return false;
+	}
+};
+
+const setCustomTemplate = (data) => {
+	Object.entries(data).forEach(([k, v]) => {
+		if (!validTemplates.includes(k)) return;
+		customTemplates[k] = v?.template;
+	});
+};
 
 export const generateCustomTemplate = (template, data = []) => {
-    let stringTemplate = JSON.stringify(template);
-    console.log(data);
-    if(stringTemplate.includes("$params$")){
-        stringTemplate = stringTemplate.replace("$params$", data);
-    }
-    return JSON.parse(stringTemplate);
-}
+	let stringTemplate = JSON.stringify(template);
+	console.log(data);
+	if (stringTemplate.includes('$params$')) {
+		stringTemplate = stringTemplate.replace('$params$', data);
+	}
+	return JSON.parse(stringTemplate);
+};
 
-const isNumber = v => typeof v === 'number' && !isNaN(v) && v
-const isString = v =>  typeof v === 'string'
-const isArray = v =>  Array.isArray(v)
-const checkMinValue = (value, minValue) => value && value.length >= minValue && value && minValue
-const checkMaxValue = (value, maxValue) => value && value.length <= maxValue && value && maxValue
-const checkEnum = (value, enums) => enums.includes(value)
-const checkEmail = (email, regex) => email && email.includes("@") && email.includes('.') && email.length > 3 && email.match(regex ? regex : regExpEmail)
-const isPositive = value => Math.sign(value) === 1;
-const isNegative = value => Math.sign(value) === -1;
+const isNumber = (v) => typeof v === 'number' && !isNaN(v) && v;
+const isString = (v) => typeof v === 'string';
+const isArray = (v) => Array.isArray(v);
+const checkMinValue = (value, minValue) =>
+	value && value.length >= minValue && value && minValue;
+const checkMaxValue = (value, maxValue) =>
+	value && value.length <= maxValue && value && maxValue;
+const checkEnum = (value, enums) => enums.includes(value);
+const checkEmail = (email, regex) =>
+	email &&
+	email.includes('@') &&
+	email.includes('.') &&
+	email.length > 3 &&
+	email.match(regex ? regex : regExpEmail);
+const isPositive = (value) => Math.sign(value) === 1;
+const isNegative = (value) => Math.sign(value) === -1;
 const testRegex = (value, regex) => value.match(regex);
 
-const regExpEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+const regExpEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
--- index.js
+++ index.js
@@ -1,189 +1,248 @@
-import { getMissingParams, checkInvalidParams, generateError, getUnknownParams, customTemplates, generateCustomTemplate, readConfig} from "./helpers.js";
+import {
+	checkInvalidParams,
+	CONFIG_PATH,
+	customTemplates,
+	generateCustomTemplate,
+	generateError,
+	getMissingParams,
+	getUnknownParams,
+	readConfig,
+} from './helpers.js';
 
 const connected_keys = [];
-const response_options = []
+const response_options = [];
 export let strictMode = false;
 let secureMode = false;
 
 const validResponseOptions = {
-    detailed: 'detailed',
-}
+	detailed: 'detailed',
+};
 
 const Response = {
-    Options: function(options) {
-        Object.entries(options).forEach(([k, v]) => {
-            if(validResponseOptions[k]) response_options.push({[k]: v})
-        })
-    },
-    GetOptions: function(){
-        return response_options
-    },
-    useCustomTemplate: function () {
-        readConfig()
-    }
-}
+	Options: function (options) {
+		Object.entries(options).forEach(([k, v]) => {
+			if (validResponseOptions[k]) response_options.push({ [k]: v });
+		});
+	},
+	GetOptions: function () {
+		return response_options;
+	},
+};
+
+const options = {
+	useCustomTemplate: function (configPath = undefined) {
+		if (typeof configPath !== 'undefined') CONFIG_PATH = configPath;
+		readConfig();
+	},
+};
 
 class Validator {
-    rules = [];
-    string(){
-        this.rules.push({rule: 'string'});
-        return this;
-    }
-    integer(){
-        this.rules.push({rule: 'integer'});
-        return this;
-    }
-    min(minValue){
-        this.rules.push({rule: 'min', value: minValue});
-        return this;
-    }
-    max(maxValue){
-        this.rules.push({rule: 'max', value: maxValue});
-        return this;
-    }
-    enum(args){
-        this.rules.push({rule: 'enum', value: args});
-        return this;
-    }
-    email(object){
-        this.rules.push({rule: 'email', ...object});
-        return this;
-    }
-    positive(){
-        this.rules.push({rule: 'positive'});
-        return this;
-    }
-    negative(){
-        this.rules.push({rule: 'negative'});
-        return this;
-    }
-    required(){
-        this.rules.push({rule: 'required'});
-        return this;
-    }
-    lowercase(){
-        this.rules.push({rule: 'lowercase'});
-        return this;
-    }
-    uppercase(){
-        this.rules.push({rule: 'uppercase'});
-        return this;
-    }
-    trim(){
-        this.rules.push({rule: 'trim'});
-        return this;
-    }
-    default(value){
-        this.rules.push({rule: 'default', value: value});
-        return this;
-    }
-    regex(value){
-        this.rules.push({rule: 'regex', value: value})
-        return this;
-    }
-    Route = {
-        Connect: function(route, schema){
-            connected_keys.push({[route]: schema});
-        }
-    }
-    Schema(){
-        return {
-            Create: function(schema){
-                return schema;
-            }
-        };
-    }
-    useStrictMode(value){
-        strictMode = !!value;
-        return this;
-    }
-    useSecureMode(value){
-        secureMode = !!value;
-        return this;
-    }
-    validateKeys = validateKeys;
-    Response = Response;
+	rules = [];
+	string() {
+		this.rules.push({ rule: 'string' });
+		return this;
+	}
+	integer() {
+		this.rules.push({ rule: 'integer' });
+		return this;
+	}
+	min(minValue) {
+		this.rules.push({ rule: 'min', value: minValue });
+		return this;
+	}
+	max(maxValue) {
+		this.rules.push({ rule: 'max', value: maxValue });
+		return this;
+	}
+	enum(args) {
+		this.rules.push({ rule: 'enum', value: args });
+		return this;
+	}
+	email(object) {
+		this.rules.push({ rule: 'email', ...object });
+		return this;
+	}
+	positive() {
+		this.rules.push({ rule: 'positive' });
+		return this;
+	}
+	negative() {
+		this.rules.push({ rule: 'negative' });
+		return this;
+	}
+	required() {
+		this.rules.push({ rule: 'required' });
+		return this;
+	}
+	lowercase() {
+		this.rules.push({ rule: 'lowercase' });
+		return this;
+	}
+	uppercase() {
+		this.rules.push({ rule: 'uppercase' });
+		return this;
+	}
+	trim() {
+		this.rules.push({ rule: 'trim' });
+		return this;
+	}
+	default(value) {
+		this.rules.push({ rule: 'default', value: value });
+		return this;
+	}
+	regex(value) {
+		this.rules.push({ rule: 'regex', value: value });
+		return this;
+	}
+	Route = {
+		Connect: function (route, schema) {
+			connected_keys.push({ [route]: schema });
+		},
+	};
+	Schema() {
+		return {
+			Create: function (schema) {
+				return schema;
+			},
+		};
+	}
+	useStrictMode(value) {
+		strictMode = !!value;
+		return this;
+	}
+	useSecureMode(value) {
+		secureMode = !!value;
+		return this;
+	}
+	validateKeys = validateKeys;
+	Response = Response;
+	Options = options;
 }
 
-export default Validator
+export default Validator;
 
-function validateKeys(req, res, next){
+function validateKeys(req, res, next) {
+	let expected_keys;
+	const keys = connected_keys;
 
-    let expected_keys
-    const keys = connected_keys
+	const path = req.originalUrl;
 
-    const path = req.originalUrl
+	Object.entries(keys).forEach(([k, schema]) => {
+		Object.entries(schema).forEach(([route, v]) => {
+			if (route === path) expected_keys = v;
+		});
+	});
 
-    Object.entries(keys).forEach(([k, schema]) => {
-        Object.entries(schema).forEach(([route, v]) => {
-            if(route === path) expected_keys = v
-        })
-    })
+	if (!expected_keys) return next();
 
-    if(!expected_keys) return next();
+	if (secureMode) {
+		const unknownParams = getUnknownParams(req.body, expected_keys);
+		if (!unknownParams.success) {
+			if (customTemplates?.unknown_params)
+				return res
+					.status(400)
+					.send(
+						generateCustomTemplate(
+							customTemplates.missing_params,
+							unknownParams.unknownParams
+						)
+					);
 
-    if(secureMode){
-        const unknownParams = getUnknownParams(req.body, expected_keys);
-        if(!unknownParams.success){
+			return res.status(400).send(
+				generateError('unknown_param(s)', {
+					unknown_params: unknownParams.unknownParams,
+				})
+			);
+		}
+	}
 
-            if(customTemplates?.unknown_params) return res.status(400).send(generateCustomTemplate(customTemplates.missing_params, unknownParams.unknownParams));
+	const missingParams = getMissingParams(req.body, expected_keys);
+	if (!missingParams.success) {
+		let useTemplate = false;
 
-            return res.status(400).send(generateError('unknown_param(s)', {unknown_params: unknownParams.unknownParams}));
-        }
-    }
+		if (customTemplates?.missing_params)
+			useTemplate = generateCustomTemplate(
+				customTemplates.missing_params,
+				missingParams.missingParams
+			);
 
-    const missingParams = getMissingParams(req.body, expected_keys)
-    if(!missingParams.success){
+		const requiredParams = [];
+		let mp = missingParams.missingParams;
+		mp.forEach((param) => {
+			const paramRules = expected_keys[param].rules;
+			paramRules.forEach((rule) => {
+				if (rule.rule === 'required') requiredParams.push(param);
+			});
+			if (paramRules.includes('required')) requiredParams.push(param);
+		});
 
-        let useTemplate = false;
+		if (strictMode) {
+			if (useTemplate) return res.status(400).send(useTemplate);
+			return res.status(400).send(
+				generateError('missing_param(s)', {
+					missing_params: missingParams.missingParams,
+				})
+			);
+		}
+		if (requiredParams.length > 0)
+			return useTemplate
+				? res.status(400).send(useTemplate)
+				: res.status(400).send(
+						generateError('missing_param(s)', {
+							missing_params: requiredParams,
+						})
+				  );
+	}
 
-        if(customTemplates?.missing_params) useTemplate = generateCustomTemplate(customTemplates.missing_params, missingParams.missingParams);
+	const invalidParams = checkInvalidParams(req.body, expected_keys);
+	if (!invalidParams.success) {
+		let renderDetailed = false;
+		let useTemplate = false;
 
-        const requiredParams = [];
-        let mp = missingParams.missingParams;
-        mp.forEach(param => {
-            const paramRules = expected_keys[param].rules;
-            paramRules.forEach(rule => {
-                if(rule.rule === 'required') requiredParams.push(param);
-            })
-            if(paramRules.includes('required')) requiredParams.push(param);
-        })
+		response_options.forEach((option) => {
+			Object.entries(option).forEach(([k, v]) => {
+				if (k === 'detailed' && v) renderDetailed = true;
+			});
+		});
 
-        if(strictMode){
-            if(useTemplate) return res.status(400).send(useTemplate);
-            return res.status(400).send(generateError('missing_param(s)', {missing_params: missingParams.missingParams}));
-        }
-        if(requiredParams.length > 0) return useTemplate ? res.status(400).send(useTemplate) : res.status(400).send(generateError('missing_param(s)', {missing_params: requiredParams}));
-    }
+		if (customTemplates?.invalid_params) useTemplate = true;
 
-    const invalidParams = checkInvalidParams(req.body, expected_keys)
-    if(!invalidParams.success) {
-        let renderDetailed = false
-        let useTemplate = false;
+		if (!renderDetailed) {
+			const invalidKeys = [];
+			invalidParams.invalidParams.forEach((k) => {
+				Object.keys(k).forEach((key) => invalidKeys.push(key));
+			});
 
-        response_options.forEach(option => {
-            Object.entries(option).forEach(([k, v]) => {
-                if(k === 'detailed' && v) renderDetailed = true
-            })
-        })
+			if (useTemplate)
+				return res
+					.status(400)
+					.send(
+						generateCustomTemplate(customTemplates.invalid_params, invalidKeys)
+					);
 
-        if(customTemplates?.invalid_params) useTemplate = true;
+			return res
+				.status(400)
+				.send(
+					generateError('invalid_param(s)', { invalid_params: invalidKeys })
+				);
+		}
 
-        if(!renderDetailed){
-            const invalidKeys = []
-            invalidParams.invalidParams.forEach(k => {
-                Object.keys(k).forEach(key => invalidKeys.push(key))
-            })
+		return useTemplate && !renderDetailed
+			? res
+					.status(400)
+					.send(
+						generateCustomTemplate(
+							customTemplates.invalid_params,
+							invalidParams.invalidParams
+						)
+					)
+			: res.status(400).send(
+					generateError('invalid_param(s)', {
+						invalid_params: invalidParams.invalidParams,
+					})
+			  );
+	}
 
-            if(useTemplate) return res.status(400).send(generateCustomTemplate(customTemplates.invalid_params, invalidKeys));
-
-            return res.status(400).send(generateError('invalid_param(s)', {invalid_params: invalidKeys}))
-        }
-
-        return (useTemplate && !renderDetailed ) ? res.status(400).send(generateCustomTemplate(customTemplates.invalid_params, invalidParams.invalidParams)) : res.status(400).send(generateError('invalid_param(s)', {invalid_params: invalidParams.invalidParams}))
-    }
-
-    req.body = invalidParams?.transformed_keys;
-    next();
-
+	req.body = invalidParams?.transformed_keys;
+	next();
 }
